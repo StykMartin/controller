250f595be8eca02c02cebc6452ce4a5cc43e7d0f
Martin Styk <mart.styk@gmail.com>
2021-11-27 20:04:48 +0100

    feat: validate FQDN before using it

--- controller/installation/service.py
+++ controller/installation/service.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import shlex
 import subprocess
 
@@ -7,11 +8,24 @@ logger = logging.getLogger(__name__)
 CLEAR_NETBOOT_BIN = "/usr/bin/beaker-clear-netboot"
 CLEAR_NETBOOT_CMD = f"sudo {CLEAR_NETBOOT_BIN} {{fqdn}}"
 
+VALID_FQDN_REGEX = (r"^(?=.{1,255}$)[0-9A-Za-z]"
+                    r"(?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z]"
+                    r"(?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?)*\.?$")
+# do this at the global scope to avoid compiling it on every call
+regex_compiled = re.compile(VALID_FQDN_REGEX)
+
+
+def is_valid_fqdn(fqdn):
+    return regex_compiled.search(fqdn)
+
 
 def clear_netboot(fqdn: str) -> None:
     """Remove netboot configuration for given FQDN."""
 
     logger.debug("clear_netboot %s", fqdn)
+
+    if not is_valid_fqdn(fqdn):
+        raise RuntimeError(f"{fqdn} is not valid FQDN.")
 
     fqdn_command = CLEAR_NETBOOT_CMD.format(fqdn=fqdn)
     full_command = shlex.split(fqdn_command)
